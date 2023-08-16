0485af7fb6c561e7ae070c2b5459056c1c1c162f
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-16 14:37:04 +0300

    loops in python

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [x ** 2 for x in int_list if isinstance(x, int)]
+    return squared_list
+
 
 def fizzbuzz():
     # code goes here!
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
     pass
